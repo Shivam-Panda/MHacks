06a6711b6cba476043f4a5fa2e4c0ea9437c54a2
Shivam Panda <panda.shivam81@bloomfield.org>
2023-02-17 20:03:57 -0500

    Init

---
+++ api/src/index.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+import { MovieResolver } from "./resolvers/MovieResolver";
+
+(async () => {
+  const app = express();
+
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver, MovieResolver]
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log("express server started");
+  });
+})();
