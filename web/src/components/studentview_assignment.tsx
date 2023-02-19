4446625b1d651ae102f40bfcfc174183e480fa5d
jeevsanp1 <100105187+jeevsanp1@users.noreply.github.com>
2023-02-18 21:41:16 -0500

    Added Key tracker
    Next detect tabs

--- web/src/components/studentview_assignment.tsx
+++ web/src/components/studentview_assignment.tsx
@@ -2,13 +2,34 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { text } from 'stream/consumers';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './component.css';
+
+//import { Configuration, OpenAIApi } from "openai";
+//const configuration = new Configuration({
+//    organization: "org-1gnCAjr84jIVS6pho8agKwZu",
+//    apiKey: process.env.OPENAI_API_KEY,
+//});
+//const openai = new OpenAIApi(configuration);
+//const response = await openai.listEngines();
+
 
 const Studentview = () => {
   const [txt, setEmail] = useState('');
+  const [state, setState] = useState('');
   const fetchApi = (txt: any) => {
     return "token";
+  }
+  const handler = (event: any) => {
+    // changing the state to the name of the key
+  // which is pressed
+  setState(event.key);
+  };
+  useEffect(()=> {
+    document.addEventListener('keydown', detectKeyDown, true)
+  })
+  const detectKeyDown = (ev: any) => {
+    console.log("Clicked Key:", ev.key)
   }
   return (
     <>
@@ -22,13 +43,8 @@ const Studentview = () => {
     />
     <div>
     <div className='Text_Entry'>
-    <TextField
-          required// hey
-          id="outlined-required"
-          label="Submit Assignment"
-          defaultValue=""
-        />
-    <Button variant="outlined" onClick={(e) => {
+    <input id="input"></input>
+    <Button variant="outlined" onClick={(e) => {// je crois en toi
       sessionStorage.setItem('token', fetchApi(txt));}}>Submit</Button>
     </div>
     </div>
@@ -39,5 +55,5 @@ const Studentview = () => {
 export default Studentview
 
 // api key : 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
-// i would never delete it bb
 // don't worry bb
+//I would never delete it bb
